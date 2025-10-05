cfff6773d56a9cea5ec4080f364daec34f3d7622
yudai-yam <yudaimadeinjapan@gmail.com>
2025-10-05 12:50:32 +0900

    env

--- src/data_catalog/catalog_builder/config.py
+++ src/data_catalog/catalog_builder/config.py
@@ -1,8 +1,9 @@
+import os
 from pathlib import Path
 
-inputs_dir = Path(__file__).parents[3].resolve() / "inputs"
+# inputs_dir = Path(__file__).parents[3].resolve() / "inputs"
 
-# inputs_dir = Path(os.getenv("input_dir", "inputs")).resolve()
+inputs_dir = Path(os.getenv("input_dir", "inputs")).resolve()
 rst_dir = Path(__file__).parents[3] / "rst_source"
 template_dir = Path(__file__).parents[1] / "templates"
 
